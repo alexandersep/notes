574e28e57b639a511854380cecba17fb847110ac
alexander <sepelena@tcd.ie>
2022-09-25 16:54:11 +0100

    Added additional functionality to autocomplete, edited README, updates notes help, edited -p flag functionality

--- autocomplete.sh
+++ autocomplete.sh
@@ -1,19 +1,22 @@
 NOTES_DIR="$HOME/Documents/Notes/"
 _notes()
 {
-  local cur prev opts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  opts="-y -s -d -O -w -r"
-  
-  case "$prev" in
-    -y) COMPREPLY=($(compgen -W "`ls $NOTES_DIR`" "${COMP_WORDS[$COMP_CWORD]}")) && year=$COMPREPLY;;
-    -s) COMPREPLY=($(compgen -W "`ls $NOTES_DIR/$year`" "${COMP_WORDS[$COMP_CWORD]}")) && sem=$COMPREPLY;;
-    -d) COMPREPLY=($(compgen -W "`ls $NOTES_DIR/$year/$sem`" "${COMP_WORDS[$COMP_CWORD]}")) && dir=$COMPREPLY;;
-    -O) COMPREPLY=($(compgen -W "`ls $NOTES_DIR/$year/$sem/$dir/*.pdf | awk -F '/' '{print $NF}' | tr '\n' ' ' && echo`" "${COMP_WORDS[$COMP_CWORD]}"));;
-  esac
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="-y -s -d -O -w -r -p"
 
+    WriteFile="ls *.md"
+    OpenPdf="ls *.pdf 2> /dev/null | awk -F '/' '{print $NF}' | tr '\n' ' '"
+
+    case "$prev" in
+        -y) COMPREPLY=($(compgen -W "`ls $NOTES_DIR`" "${COMP_WORDS[$COMP_CWORD]}")) && year=$COMPREPLY;;
+        -s) COMPREPLY=($(compgen -W "`ls $NOTES_DIR/$year`" "${COMP_WORDS[$COMP_CWORD]}")) && sem=$COMPREPLY;;
+        -d) COMPREPLY=($(compgen -W "`ls $NOTES_DIR/$year/$sem`" "${COMP_WORDS[$COMP_CWORD]}")) && dir=$COMPREPLY;;
+        -p|-O) COMPREPLY=($(compgen -W "`[[ $(eval "$OpenPdf") == "" ]] || echo $(eval "$OpenPdf")`" "${COMP_WORDS[$COMP_CWORD]}"));;
+        -w|-r) COMPREPLY=($(compgen -W "`[[ $(eval "$WriteFile") == "" ]] || echo $(ls *.md)`" "${COMP_WORDS[$COMP_CWORD]}"));;
+    esac
 }
 complete -o nospace -F _notes notes
 complete -o nospace -F _notes ./notes
